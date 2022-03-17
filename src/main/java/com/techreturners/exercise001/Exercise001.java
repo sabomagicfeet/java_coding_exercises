08d7140b5f5af046e030be1e5396b3c5d4e0eae0
sabo <sabo@jihyes-mbp.home>
2022-03-17 11:51:58 +0000

    add capitalizedWord and generateInitials

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -15,7 +15,13 @@ public class Exercise001 {
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        String firstNameInitial = String.valueOf(firstName.charAt(0));
+        String lastNameInitial = String.valueOf(lastName.charAt(0));
+        String initial = firstNameInitial + "." + lastNameInitial;
+
+        System.out.println(initial);
+
+        return initial;
     }
 
     public double addVat(double originalPrice, double vatRate) {
